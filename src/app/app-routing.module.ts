f79f481508babed45711ee4581a14f8a4abfe252
Cris <petrixia@proton.me>
2023-05-31 20:08:58 -0300

    build: content c

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ArticlesComponent } from './pages/articles/articles.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { ContentComponent } from './pages/content/content.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
-  { path: 'articles', component: ArticlesComponent },
-  { path: 'contact', component: ContactComponent },
+  { path: '/about', component: AboutComponent },
+  { path: '/articles', component: ArticlesComponent },
+  { path: '/contact', component: ContactComponent },
+  { path: '/component', component: ContentComponent}
 ];
 
 @NgModule({
@@ -18,3 +20,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+  
