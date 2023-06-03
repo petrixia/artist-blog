98e3a46e15ce4850bbe3fea527e28ca7e4391d29
Cris <petrixia@proton.me>
2023-06-02 21:49:23 -0300

    bug: tattoo form

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  tattooForm: any = FormGroup
 
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(): void {
+    this.tattooForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      country: ['', Validators.required],
+      uf: ['', Validators.required],
+      city: ['', Validators.required],
+      birthday: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      instagram: [''],
+      tattooDescription: ['', Validators.required],
+      bodyPart: ['', Validators.required],
+      size: ['', Validators.required],
+      howDidYouMeet: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.tattooForm.valid) {
+      // Submit the form data
+      console.log(this.tattooForm.value);
+      // You can send the data to an API, perform validation, etc.
+    } else {
+      // Display error messages or handle form validation
+    }
+  }
 }
